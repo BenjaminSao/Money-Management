5d7b3e88aff45e61175d5578130f1986235f3671
abygeorge7 <aby@sofdev.io>
2021-07-09 02:39:36 +0530

    done the initial design

--- main.js
+++ main.js
@@ -1,11 +1,70 @@
-var app = new Vue({
-    el: "#app",
-    data: {
-        nums: []
-    },
-    methods: {
-        addNum() {
-            this.nums.push(Math.round(Math.random() * 10));
+Vue.component('Transactions', {
+    template: `
+    
+    <form action="" @submit.prevent="onSubmit" class ="field has-addons">
+     <input type="text" class="input is-primary" v-model= "name" placeholder = " Enter Name of transaction">
+     <input type="text" class="input is-primary" id="description" v-model= "description"placeholder = " Enter Details">
+     <div class ="select is-normal is-primary">
+    <select  v-model= "type" >
+        <option selected >Credit</option>
+        <option>Debit</option>
+    </select>
+    </div>
+     <input type="number" class="input is-primary" v-model.number="amount" placeholder = " Enter Amount">
+     
+    <input type="submit" class="button is-primary" value="Add">  
+     
+</form>
+    `,
+    data() {
+        return {
+            name: null,
+            description: null,
+            type: null,
+            amount: null,
+
+
         }
     },
-});
+    methods: {
+        onSubmit() {
+            let transactionList = {
+                name: this.name,
+                description: this.description,
+                type: this.type,
+                amount: this.amount
+            }
+            console.log(transactionList.name)
+            this.$emit('transaction-submitted', transactionList)
+            this.name = null
+            this.description = null
+            this.type = null
+            this.amount = null
+        },
+
+    }
+})
+
+
+
+
+
+var app = new Vue
+    ({
+        el: "#app",
+        data: {
+            title: "Expense Tracker",
+            netWorth: "Income",
+            Expense: "Expense",
+            finalTransaction: []
+        },
+        methods: {
+            addTransaction(transactionList) {
+
+                this.finalTransaction.push(transactionList)
+                console.log(this.finalTransaction)
+            }
+        }
+
+
+    });
