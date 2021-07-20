789e40b70ce4f2fee7fd6d8121e4b7f5e1cb3952
Benjamin Saobuppha <ben.saobuppha@mail.utoronto.ca>
2021-07-20 18:03:03 -0400

    minor refactoring, formatted code with prettier

--- main.js
+++ main.js
@@ -10,27 +10,28 @@ Vue.component("transaction",
     data()
     {
         return {
-            id:null,
-            name:null,
-            description:null,
-            type:null,
-            money:null,
-            amount:null,
+            id: null,
+            name: null,
+            description: null,
+            type: null,
+            money: null,
+            amount: null,
         }
     },
 
     watch:
     {
-        editTransactionId:function(_editTransactionId)
+        editTransactionId: function(_editTransactionId)
         {
             if (_editTransactionId)
             {
-                const transaction=this.transactions.find(t=>t.id===_editTransactionId);
-                this.name=transaction.name;
-                this.description=transaction.description;
-                this.type=transaction.type;
-                this.money=transaction.money;
-                this.amount=transaction.amount;    
+                const transaction = this.transactions.find(t => t.id === _editTransactionId);
+
+                this.name = transaction.name;
+                this.description = transaction.description;
+                this.type = transaction.type;
+                this.money = transaction.money;
+                this.amount = transaction.amount;    
             }
         }
     },
@@ -39,38 +40,42 @@ Vue.component("transaction",
     {
         onSubmit()
         {
-            if (this.editTransactionId) {
-                transaction=this.transactions.find(t=>t.id===this.editTransactionId);
-                transaction.name=this.name;
-                transaction.description=this.description;
-                transaction.type=this.type;
-                transaction.money=this.money;
-                transaction.amount=this.amount;
-                this.$emit("transaction-edited");
-                this.$emit("transaction-updated");
-                this.name=null;
-                this.description=null;
-                this.type=null;
-                this.money=null,
-                this.amount=null;
+            if (this.editTransactionId) 
+            {
+                transaction = this.transactions.find(t => t.id === this.editTransactionId);
+                transaction.name = this.name;
+                transaction.description = this.description;
+                transaction.type = this.type;
+                transaction.money = this.money;
+                transaction.amount = this.amount;
+
+                this.name = null;
+                this.description = null;
+                this.type = null;
+                this.money = null,
+                this.amount = null;
             }
-            else {
-                let transaction={
-                    id:Date.now(),
-                    name:this.name,
-                    description:this.description,
-                    type:this.type,
-                    money:this.money,
-                    amount:this.amount
+            else 
+            {
+                let transaction = {
+                    id: Date.now(),
+                    name: this.name,
+                    description: this.description,
+                    type: this.type,
+                    money: this.money,
+                    amount: this.amount
                 }
+
                 this.$emit("transaction-submitted",transaction);
-                this.$emit("transaction-updated");
-                this.name=null;
-                this.description=null;
-                this.type=null;
-                this.money=null;
-                this.amount=null;
+                
+                this.name = null;
+                this.description = null;
+                this.type = null;
+                this.money = null;
+                this.amount = null;
             }
+
+            this.$emit("transaction-updated");
         },   
     },
 
@@ -88,39 +93,40 @@ Vue.component("transaction",
 
 var app=new Vue
     ({
-        el:"#app",
-        data:{
-            title:"Expense Tracker",
-            expenseTotal:0,
-            incomeTotal:0,
-            tempExpenseTotal:0,
-            tempIncomeTotal:0,        
-            transactions:[],
-            editTransactionId:null,
-            currencyType:"CAD",
+        el: "#app",
+        data: 
+        {
+            title: "Expense Tracker",
+            expenseTotal: 0,
+            incomeTotal: 0,
+            tempExpenseTotal: 0,
+            tempIncomeTotal: 0,        
+            transactions: [],
+            editTransactionId: null,
+            currencyType: "CAD",
         },
 
         methods:
         {
             addTransaction(transaction)
             {
-
                 this.transactions.push(transaction);
             },
             calculateIncome()
             {
-                let temp=0;
+                let temp = 0;
+                
                 if (this.transactions)
                 {
                     for (let item in this.transactions) {
-                        if ((this.transactions[item].type)==='Credit')
+                        if ((this.transactions[item].type) === "Credit")
                         {
-                            if (this.transactions[item].money==="CAD")
-                                temp=temp + this.transactions[item].amount;
-                            if (this.transactions[item].money==="INR")
-                                temp=temp + (this.transactions[item].amount)/50;
-                            if (this.transactions[item].money==="THB")
-                                temp+=(this.transactions[item].amount)/25;
+                            if (this.transactions[item].money === "CAD")
+                                temp += this.transactions[item].amount;
+                            if (this.transactions[item].money === "INR")
+                                temp += (this.transactions[item].amount) / 50;
+                            if (this.transactions[item].money === "THB")
+                                temp += (this.transactions[item].amount) / 25;
                         }
                     }
                     this.tempIncomeTotal=temp;
@@ -130,50 +136,57 @@ var app=new Vue
             calculateExpense()
             {
                 let temp=0;
-                    for(let item in this.transactions) {
-                        if((this.transactions[item].type)==='Debit')
-                        {
-                            if(this.transactions[item].money==="CAD")
-                                temp=temp+this.transactions[item].amount;
-                            if(this.transactions[item].money==="INR")
-                                temp=temp+(this.transactions[item].amount)/50;
-                            if(this.transactions[item].money === "THB")
-                                temp+=(this.transactions[item].amount)/25;
-                        }    
-                    }
+
+                for (let item in this.transactions) {
+                    if ((this.transactions[item].type) === "Debit")
+                    {
+                        if (this.transactions[item].money === "CAD")
+                            temp += this.transactions[item].amount;
+                        if (this.transactions[item].money === "INR")
+                            temp += (this.transactions[item].amount) / 50;
+                        if (this.transactions[item].money === "THB")
+                            temp+= (this.transactions[item].amount) / 25;
+                    }    
+                }
+
                 this.tempExpenseTotal=temp;
             },
 
             changeCurrency()
             {
-                if(this.currencyType==='CAD')
+                if(this.currencyType === "CAD")
                 {
-                    this.expenseTotal=this.tempExpenseTotal;
-                    this.incomeTotal=this.tempIncomeTotal;
+                    this.expenseTotal = this.tempExpenseTotal;
+                    this.incomeTotal = this.tempIncomeTotal;
                 }
-                if (this.currencyType==='THB')
+                if (this.currencyType === "THB")
                 {
-                    this.expenseTotal=this.tempExpenseTotal*25;
-                    this.incomeTotal=this.tempIncomeTotal*25;
+                    this.expenseTotal = this.tempExpenseTotal * 25;
+                    this.incomeTotal = this.tempIncomeTotal * 25;
                 }
-                if (this.currencyType==='INR')
+                if (this.currencyType === "INR")
                 {
-                    this.expenseTotal=this.tempExpenseTotal*50;
-                    this.incomeTotal=this.tempIncomeTotal*50;
+                    this.expenseTotal = this.tempExpenseTotal * 50;
+                    this.incomeTotal = this.tempIncomeTotal * 50;
                 }
             },
 
             deleteTransaction(id)
             {
-                if(confirm("Press ok to confirm")) {
-                    for (let i=0;i<this.transactions.length;i++) {
-                        if (this.transactions[i].id===id)
+                if (confirm("Press ok to confirm")) 
+                {
+                    for (let i=0; i<this.transactions.length; i++) 
+                    {
+                        if (this.transactions[i].id === id)
                         {
-                            this.transactions.splice(i,1);
+                            this.transactions.splice(i, 1);
                         }
                     }
                 }
-                this.editTransactionId=null;         
+                
+                this.editTransactionId = null;
+                
+                this.transactionUpdated();    
             },
 
             transactionUpdated()
@@ -181,32 +194,30 @@ var app=new Vue
                 this.calculateIncome();
                 this.calculateExpense();
                 this.changeCurrency();
+
+                this.editTransactionId = null;
             },
 
             editTransaction(transaction)
             {
-                this.editTransactionId=transaction.id;
-            },
-            
-            UpdateID()
-            {
-                this.editTransactionId=null;
-            },    
+                this.editTransactionId = transaction.id;
+            }, 
         },
 
-        computed:{
+        computed:
+        {
             netInHand()
             {
-                return (this.incomeTotal-this.expenseTotal);
+                return (this.incomeTotal - this.expenseTotal);
             },
 
             currency()
             {
-                if (this.currencyType==='CAD')
+                if (this.currencyType === 'CAD')
                     return "$"
-                if (this.currencyType==='INR')
+                if (this.currencyType === 'INR')
                     return "₹"
-                if (this.currencyType==='THB')
+                if (this.currencyType === 'THB')
                     return "฿" 
             },
         },
